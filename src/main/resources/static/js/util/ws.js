4594a2eef55e7f86e5f823f9659d6be25d56c601
GuitaristNSK <GuitaristNSK@yandex.ru>
2021-08-19 16:18:21 +0700

    vue style; frontend refactoring;

--- src/main/resources/static/js/util/ws.js
+++ src/main/resources/static/js/util/ws.js
@@ -5,8 +5,11 @@ let stompClient = null
 const handlers = []
 
 export function connect() {
-    const socket = new SockJS('/gs-guide-websocket')
-    stompClient = Stomp.over(socket)
+    // const socket = new SockJS('/gs-guide-websocket')
+    // stompClient = Stomp.over(socket)
+    stompClient = Stomp.over(function () {
+        return new SockJS('/gs-guide-websocket')
+    })
     stompClient.debug = () => {}
     stompClient.connect({}, frame => {
         stompClient.subscribe('/topic/activity', message => {
